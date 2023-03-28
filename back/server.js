d58d920999b437d1bdcfb89736e75f902fb4c231
Angular0527 <Angular0527@outlook.com>
2023-03-28 14:43:48 +0200

    dev: server serveIndex

--- back/server.js
+++ back/server.js
@@ -1,1 +1,25 @@
-console.log("The server is running...");
+console.log("The server is running...")
+
+const express = require("express")
+const serveIndex = require("serve-index")
+
+const app = express()
+const port = 3000
+
+const publicDir = "."
+
+app.use((req, res, next) => {
+  console.log(req.method, req.path)
+  next()
+})
+
+app.use(express.static(publicDir))
+app.use(serveIndex(publicDir, { icons: true }))
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
